cfd3c60cfb50f8799b5a818ac879e07d0f1d222c
Amy Banka <abanka@email.arizona.edu>
2021-10-11 11:40:34 -0700

    100%

--- assignments/06_common/common.py
+++ assignments/06_common/common.py
@@ -5,9 +5,9 @@ Date   : 2021-10-10
 Purpose: I find the words in common between two files
 """
 import argparse
-import os
 import sys
 import re
+
 
 # --------------------------------------------------
 def get_args():
@@ -26,15 +26,13 @@ def get_args():
                         help='Input file 2',
                         metavar='FILE',
                         type=argparse.FileType('rt'))
- #this outputs a class '_io.TextIOWrapper'
+
     parser.add_argument('-o',
                         '--outfile',
                         help='Output filename',
                         metavar='FILE',
                         type=argparse.FileType('wt'),
-                        default = sys.stdout)
-        #this outputs a class '_io.TextIOWrapper'
- 
+                        default=sys.stdout)
 
     return parser.parse_args()
 
@@ -46,31 +44,25 @@ def main():
     args = get_args()
     contents1 = args.FILE1.read()
     contents2 = args.FILE2.read()
-    # produces a string
 
     words1 = contents1.split()
-        # produces a list
-
 
     wordlist = []
     for x in words1:
-        if x in contents2:
-            wordlist.append(x)
+        pattern = re.compile((x) + r'[\s,.]')
+        overlap = pattern.search(contents2)
+        if overlap:
+
+            word = overlap.group(0)
+
+            wordlist.append(word.rstrip())
+
     wordlist.sort()
     no_duplicates = list(dict.fromkeys(wordlist))
     final = "\n".join(no_duplicates)
     args.outfile.write(final)
     args.outfile.close()
-    
 
-
-
-    #tried a bunch of stuff with re.
-        # overlap = re.search(x, contents2)
-        # if overlap:
-        #     word = overlap.group(0)
-           # word = string
-  
 
 # --------------------------------------------------
 if __name__ == '__main__':
